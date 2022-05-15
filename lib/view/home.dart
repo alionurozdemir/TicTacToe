d01763639ddb13e2500c1d6f663dc9eeaa64088d
Ali Onur Özdemir <alionurozdemir1@gmail.com>
2022-05-15 20:59:15 +0300

    Stack bug fixed

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:tic_tac_toe/view/game_panel.dart';
 import 'package:tic_tac_toe/view/players_info.dart';
 
 class Home extends StatefulWidget {
@@ -13,46 +12,39 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Welcome to TT"),
-      ),
-      body: Column(
-        children: [
-          Stack(
-            alignment: Alignment.center,
-            clipBehavior: Clip.none,
-            children: [
-              SizedBox(height: 395, child: Image.asset("assets/images/background.png")),
-              Positioned(
+        appBar: AppBar(
+          title: const Text("Welcome to TT"),
+        ),
+        body: Column(children: [
+          Stack(alignment: Alignment.center, clipBehavior: Clip.none, children: [
+            SizedBox(height: 395, child: Image.asset("assets/images/background.png")),
+            Positioned(
                 top: 150,
                 left: 135,
                 child: Container(
-                  width: 127,
-                  height: 100,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage("assets/images/words.png"),
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-                ),
-              ),
-              const Positioned(
+                    width: 127,
+                    height: 100,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/images/words.png"), fit: BoxFit.contain),
+                    ))),
+            const Positioned(
                 top: 375,
                 child: CircleAvatar(
-                  backgroundColor: Colors.orange,
-                  radius: 50,
-                  child: Text(
-                    'V 1.0',
-                    style:
-                        TextStyle(fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 500,
-                child: GestureDetector(
+                    backgroundColor: Colors.orange,
+                    radius: 50,
+                    child: Text(
+                      'V 1.0',
+                      style:
+                          TextStyle(fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),
+                    )))
+          ]),
+          const Spacer(),
+          Align(
+              alignment: Alignment.bottomCenter,
+              child: GestureDetector(
                   onTap: () {
+                    print('ss');
                     Navigator.push(
                         context, MaterialPageRoute(builder: ((context) => const PlayersInfo())));
                   },
@@ -60,13 +52,8 @@ class _HomeState extends State<Home> {
                     "Continue >>",
                     style:
                         TextStyle(fontSize: 50, fontWeight: FontWeight.bold, color: Colors.green),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ],
-      ),
-    );
+                  ))),
+          const Spacer(),
+        ]));
   }
 }
